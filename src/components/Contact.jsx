3038121efff123f84dafdfaadec5f31aaa863a8d
tosinehindero <tosin.ehindero@gmail.com>
2024-09-20 14:19:17 -0400

    added new resume link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,7 +30,7 @@ const Contact = () => {
           {CONTACT.email}
         </a>
         <a
-          href="https://resume-portfoliobk.s3.amazonaws.com/Resume.pdf" // Update with the actual path to your resume
+          href="https://tosinehindero.com.amazonaws.com/Resume.pdf" // Update with the actual path to your resume
           download
           className="mt-8 inline-block rounded bg-orange-700 px-6 py-3 text-white hover:bg-purple-800 transition-colors"
         >
